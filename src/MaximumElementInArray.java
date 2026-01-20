d9367094ab002d96569aceafb77f3d11815b80b9
FansopkarMehvish <mehvishfansopkar8@gmail.com>
2026-01-20 10:07:05 +0530

    Maximum Element in Array using Math.max

--- src/MaximumElementInArray.java
+++ src/MaximumElementInArray.java
@@ -5,9 +5,10 @@ public class MaximumElementInArray {
     public static int maximumElementOfArray(int[] arr){
         int max = Integer.MIN_VALUE;
         for(int i=0; i < arr.length; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+//            if(arr[i] > max){
+//                max = arr[i];
+//            }
+            max = Math.max(max, arr[i]);
         }
         return max;
     }
