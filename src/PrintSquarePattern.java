ef84e7f2b4796740567ed2cddc82c439edaca70f
FansopkarMehvish <mehvishfansopkar8@gmail.com>
2026-01-22 19:22:51 +0530

    Print square pattern

---
+++ src/PrintSquarePattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PrintSquarePattern {
+
+    public static void printSquarePattern(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        printSquarePattern(n);
+    }
+}
